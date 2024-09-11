f62ccd0182c3e5b1c287e054f576662d9a100b2e
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2024-09-11 15:00:13 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-// Import the functions you need from the SDKs you need
+// Firebase Initialization
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js';
+import {
+	getAuth,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	signOut,
+} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
+import {
+	getDatabase,
+	ref,
+	set,
+} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js';
+import {
+	getStorage,
+	ref as storageRef,
+	uploadBytes,
+	getDownloadURL,
+} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyCRWH9mw1Cfxh2-batz6cXeUkT_vMi8la4',
 	authDomain: 'wkmn-link-track.firebaseapp.com',
@@ -7,115 +25,162 @@ const firebaseConfig = {
 	projectId: 'wkmn-link-track',
 	storageBucket: 'wkmn-link-track.appspot.com',
 	messagingSenderId: '868512692736',
-	appId: '1:868512692736:web:98882b62310bc7e444b6e5'
+	appId: '1:868512692736:web:98882b62310bc7e444b6e5',
 };
+
 const app = initializeApp(firebaseConfig);
-import {
-	getAuth,
-	signInWithEmailAndPassword,
-	onAuthStateChanged,
-	signOut
-} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
-import {
-	getDatabase,
-	ref,
-	set
-} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js';
 const database = getDatabase(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-const userEmail = document.querySelector('#userEmail');
-const userPassword = document.querySelector('#userPassword');
-const authForm = document.querySelector('#authForm');
-const secretContent = document.querySelector('#secretContent');
-const shortView = document.querySelector('#shortView');
-// const signUpButton = document.querySelector('#signUpButton');
-const signInButton = document.querySelector('#signInButton');
-const signOutButton = document.querySelector('#signOutButton');
-const longURL = document.querySelector('#longURL');
-const createRedirxButton = document.querySelector('#createURLButton');
+// Utility Functions
+const selectElement = (id) => document.querySelector(id);
 
-secretContent.style.display = 'none';
-shortView.style.display = 'none';
+const toggleElementDisplay = (element, displayState) => {
+	element.style.display = displayState;
+};
 
-// async function userSignUp() {
-// 	const userEmailValue = userEmail.value;
-// 	const userPasswordValue = userPassword.value;
-// 	try {
-// 		const userCredential = await createUserWithEmailAndPassword(
-// 			auth,
-// 			userEmailValue,
-// 			userPasswordValue
-// 		);
-// 		const user = userCredential.user;
-// 		// checkAuthState();
-// 		console.log(user);
-// 	} catch (error) {
-// 		alert(error.code + ': ' + error.message);
-// 	}
-// }
+const handleError = (error) => {
+	alert(`${error.code}: ${error.message}`);
+};
+
+// DOM Elements
+const userEmail = selectElement('#userEmail');
+const userPassword = selectElement('#userPassword');
+const authForm = selectElement('#authForm');
+const secretContent = selectElement('#secretContent');
+const shortView = selectElement('#shortView');
+const signInButton = selectElement('#signInButton');
+const signOutButton = selectElement('#signOutButton');
+const longURL = selectElement('#longURL');
+const createRedirxButton = selectElement('#createURLButton');
+const fileUpload = selectElement('#fileUpload');
+const uploadFileButton = selectElement('#uploadFileButton');
+const orCreateFile = selectElement('#orCreateFile');
+const fileUploadView = selectElement('#fileUploadView');
+const shortURL = selectElement('#shortURL');
+const refreshButton = selectElement('#refreshPage');
+
+// Initial View Setup
+toggleElementDisplay(secretContent, 'none');
+toggleElementDisplay(shortView, 'none');
+toggleElementDisplay(fileUploadView, 'none');
+
+// Authentication Functions
 async function userSignIn() {
-	const userEmailValue = userEmail.value;
-	const userPasswordValue = userPassword.value;
 	try {
 		const userCredential = await signInWithEmailAndPassword(
 			auth,
-			userEmailValue,
-			userPasswordValue
+			userEmail.value,
+			userPassword.value
 		);
-		const user = userCredential.user;
-
-		console.log(user);
+		console.log(userCredential.user);
 	} catch (error) {
-		alert(error.code + ': ' + error.message);
+		handleError(error);
 	}
 }
+
 async function userSignOut() {
 	try {
 		await signOut(auth);
 	} catch (error) {
-		alert(error.code + ': ' + error.message);
+		handleError(error);
 	}
 }
-async function createRedirx() {
-	//  in rtdb create randomSixCharCode>allowed_view: "", url: "longURL" and return the randomSixCharCode
-	const longURLValue = longURL.value;
-	const randomSixCharCode = Math.random().toString(36).substring(2, 8);
-	const allowed_view = '0';
-	const redirxData = {
-		allowed_view: allowed_view,
-		url: longURLValue,
-	};
-	try {
-		await set(ref(database, 'link_track/' + randomSixCharCode), redirxData);
-		console.log(randomSixCharCode);
-		shortView.style.display = 'block';
-		secretContent.style.display = 'none';
-		document.querySelector(
-			'#shortURL'
-		).innerHTML = `https://rdrx.top/${randomSixCharCode}`;
-		document.querySelector(
-			'#shortURL'
-		).href = `https://rdrx.top/${randomSixCharCode}`;
-		document.querySelector('#img').src =
-			'https://qr.redirx.top/?url=' + 'https://rdrx.top/' + randomSixCharCode;
-	} catch (error) {
-		alert(error.code + ': ' + error.message);
-	}
-}
-async function checkAuthState() {
+
+// Auth State Change Listener
+function checkAuthState() {
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
-			secretContent.style.display = 'block';
-			authForm.style.display = 'none';
+			toggleElementDisplay(secretContent, 'block');
+			toggleElementDisplay(authForm, 'none');
 		} else {
-			secretContent.style.display = 'none';
-			authForm.style.display = 'block';
+			toggleElementDisplay(secretContent, 'none');
+			toggleElementDisplay(authForm, 'block');
 		}
 	});
 }
-checkAuthState();
-// signUpButton.addEventListener('click', userSignUp);
+
+// Database Functions
+async function createRedirxURL(longURLValue, type) {
+	const randomSixCharCode = type + Math.random().toString(36).substring(2, 8);
+	const redirxData = {
+		allowed_view: '0',
+		url: longURLValue,
+	};
+
+	try {
+		await set(ref(database, 'link_track/' + randomSixCharCode), redirxData);
+		return randomSixCharCode;
+	} catch (error) {
+		handleError(error);
+	}
+}
+
+async function createRedirx() {
+	const longURLValue = longURL.value;
+	const shortCode = await createRedirxURL(longURLValue);
+	if (shortCode) {
+		toggleElementDisplay(shortView, 'block');
+		toggleElementDisplay(secretContent, 'none');
+		shortURL.innerHTML = `https://rdrx.top/${shortCode}`;
+		shortURL.href = `https://rdrx.top/${shortCode}`;
+		selectElement(
+			'#img'
+		).src = `https://qr.redirx.top/?url=https://rdrx.top/${shortCode}`;
+	}
+}
+
+function refreshPage() {
+	window.location.reload();
+}
+
+// Firebase Storage Functions
+async function uploadFileToFirebase(file) {
+	const fileName = file.name;
+	const storageReference = storageRef(storage, 'rdrx-file-upload/' + fileName);
+
+	try {
+		const snapshot = await uploadBytes(storageReference, file);
+		return await getDownloadURL(snapshot.ref);
+	} catch (error) {
+		handleError(error);
+	}
+}
+
+async function createFileShortURL(downloadURL) {
+	const shortCode = await createRedirxURL(downloadURL, 'f-');
+	if (shortCode) {
+		toggleElementDisplay(shortView, 'block');
+		toggleElementDisplay(fileUploadView, 'none');
+		shortURL.innerHTML = `https://rdrx.top/${shortCode}`;
+		shortURL.href = `https://rdrx.top/${shortCode}`;
+	}
+}
+
+// Event Listeners
 signInButton.addEventListener('click', userSignIn);
 signOutButton.addEventListener('click', userSignOut);
 createRedirxButton.addEventListener('click', createRedirx);
+refreshButton.addEventListener('click', refreshPage);
+
+orCreateFile.addEventListener('click', () => {
+	toggleElementDisplay(fileUploadView, 'block');
+	toggleElementDisplay(secretContent, 'none');
+});
+
+uploadFileButton.addEventListener('click', async (e) => {
+	e.preventDefault();
+	const file = fileUpload.files[0];
+	if (file) {
+		const downloadURL = await uploadFileToFirebase(file);
+		if (downloadURL) {
+			await createFileShortURL(downloadURL);
+		}
+	} else {
+		alert('No file selected!');
+	}
+});
+
+// Initialize
+checkAuthState();
